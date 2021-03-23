d2ea6dfc383303612bdd49f4805eca34cdb993a5
jeonyeonkyu <ehb6915@naver.com>
2021-03-23 15:22:55 +0900

    Add: static 경로 추가해주는 미들웨어에 dist폴더 추가

--- vendingMachine/app.js
+++ vendingMachine/app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
