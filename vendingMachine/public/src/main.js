bca77f6b6c978685a5bc1f36fbfb615497461b02
jeonyeonkyu <ehb6915@naver.com>
2021-03-23 13:27:40 +0900

    Add: 스켈레콘 클래스 추가

--- vendingMachine/public/src/main.js
+++ vendingMachine/public/src/main.js
@@ -1,4 +1,34 @@
+import ProductModel from './Models/ProductModel.js';
+import ProcessModel from './Models/ProcessModel.js';
+import WalletModel from './Models/WalletModel.js';
 
-const a = [1, 2, 3, [44, 22]];
-const b = a.flat();
-console.log(b);
+import ProductView from './Views/ProductView.js';
+import ProcessView from './Views/ProcessView.js';
+import WalletView from './Views/WalletView.js';
+
+
+const walletModel = new WalletModel(23550);
+
+walletModel.init()
+walletModel.getMoneyObject(); //이렇게하면 {10 : 0, 50: 0} 요런식으로나오게 하기
+
+
+const productView = new ProductView();
+
+const processView = new ProcessView();
+
+const walletView = new WalletView();
+
+
+
+const wallet = document.querySelector(".wallet");
+wallet.addEventListener("click", (e) => {
+  const currency = e.target;
+  if (currency.innerText[currency.innerText.length - 1] === "원") {
+    let currencyCount = Number(e.target.nextElementSibling.innerText);
+    if (currencyCount > 0) {
+      currencyCount--;
+      e.target.nextElementSibling.innerText = currencyCount;
+    }
+  }
+})
