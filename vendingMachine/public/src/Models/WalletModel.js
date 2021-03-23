0b2f6679c284589e9ce2c2051fdfc7d8de69ee63
jeonyeonkyu <ehb6915@naver.com>
2021-03-23 15:03:03 +0900

    Feat: 돈을 받아서 객체로 만들어주는 walletModel의 함수 구현

--- vendingMachine/public/src/Models/WalletModel.js
+++ vendingMachine/public/src/Models/WalletModel.js
@@ -1,7 +1,44 @@
 class WalletModel {
-  constructor() {
+  moneyObject = {
+    KRW10: 0,
+    KRW50: 0,
+    KRW100: 0,
+    KRW500: 0,
+    KRW1000: 0,
+    KRW5000: 0,
+    KRW10000: 0
+  };
+  constructor(money) {
+    this.init(money);
+  }
 
+  init(money) {
+    this.makeObject(money);
+  }
+
+  makeObject(money) {
+    let currentMoney = money;
+    let twoOrFiveFlag = true;
+    let moneyKind = 10000;
+    while (currentMoney) {
+      if (currentMoney < 10) return;
+      const count = Math.floor(currentMoney / moneyKind);
+      this.moneyObject[`KRW${moneyKind}`] = count;
+      currentMoney -= count * moneyKind;
+      moneyKind = twoOrFiveFlag ? moneyKind / 2 : moneyKind / 5;
+      twoOrFiveFlag = !twoOrFiveFlag;
+    }
+  }
+
+  useMoney(moneyKind) {
+    this.moneyObject[`KRW${moneyKind}`]--;
+  }
+
+  getMoneyObject() {
+    return [...this.moneyObject];
   }
 }
+
+const walletModel = new WalletModel(23550);
 
 export default WalletModel;
