5cc7d34ebff41faf9ef3955042d3bd589a92b81e
jeonyeonkyu <ehb6915@naver.com>
2021-03-23 18:58:01 +0900

    Feat: 상품이 없어지면 상품의 색깔을 변환시켜주는 itemDisableChanger함수 구현

--- vendingMachine/public/src/Views/ProductView.js
+++ vendingMachine/public/src/Views/ProductView.js
@@ -1,6 +1,38 @@
-class ProductView {
-  constructor() {
+import _ from "../utils/elementUtil.js";
 
+class ProductView {
+  constructor({ productModel }, { $wrapper, nameListClassName, $nameList, $priceList }) {
+    this.productModel = productModel;
+    this.$wrapper = $wrapper;
+    this.nameListClassName = nameListClassName;
+    this.$nameList = $nameList;
+    this.$priceList = $priceList;
+    this.init();
+  }
+
+  init() {
+    this.initEvent();
+  }
+
+  initEvent() {
+    _.on(this.$wrapper, 'click', ({ target }) => {
+      if (!target.classList.contains(this.nameListClassName)) return;
+      this.productModel.sold(target.innerText);
+      this.render();
+    })
+  }
+
+  itemDisableChanger() {
+    const productArray = this.productModel.getProduct();
+    for (let i = 0; i < this.$nameList.length; i++) {
+      const $name = this.$nameList[i];
+      productArray[i].count === 0 ?
+        $name.classList.add('disable') : $name.classList.remove('disable');
+    }
+  }
+
+  render() {
+    this.itemDisableChanger();
   }
 }
 
