0167c4e4fb6e307db80752572cffe3146ea52f21
jeonyeonkyu <ehb6915@naver.com>
2021-03-23 18:55:18 +0900

    Feat: 상품 클릭하면 상품의 개수를 소모하는 sold함수 구현

--- vendingMachine/public/src/Models/ProductModel.js
+++ vendingMachine/public/src/Models/ProductModel.js
@@ -1,27 +1,15 @@
 class ProductModel {
-  productArray = [
-    { name: '콜라', price: '500', count: 0 },
-    { name: '사이다', price: '1000' },
-    { name: '파인애플맛 환타', price: '400' },
-    { name: '포도맛 환타', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-    { name: '사이다', price: '1000' },
-  ]
+  constructor({ products }) {
+    this.products = products;
+  }
 
+  sold(name) {
+    const item = this.products.find(item => item.name === name);
+    item.count--;
+  }
 
-
-  constructor() {
-
+  getProduct() {
+    return [...this.products];
   }
 }
 
