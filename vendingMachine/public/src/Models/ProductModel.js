14560c94e6478e3ed68b861732855036a15a7f2f
jeonyeonkyu <ehb6915@naver.com>
2021-03-23 17:55:00 +0900

    Add: 데이터 추가

--- vendingMachine/public/src/Models/ProductModel.js
+++ vendingMachine/public/src/Models/ProductModel.js
@@ -1,4 +1,25 @@
 class ProductModel {
+  productArray = [
+    { name: '콜라', price: '500', count: 0 },
+    { name: '사이다', price: '1000' },
+    { name: '파인애플맛 환타', price: '400' },
+    { name: '포도맛 환타', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+    { name: '사이다', price: '1000' },
+  ]
+
+
+
   constructor() {
 
   }
