1db06d4b6c7cdc4ac885cf5e1f3259b4ba90edfe
jeonyeonkyu <ehb6915@naver.com>
2021-03-25 01:18:59 +0900

    Add: Model, element 넣어주고 틀잡기

--- vendingMachine/public/src/Views/RepaymentView.js
+++ vendingMachine/public/src/Views/RepaymentView.js
@@ -1,10 +1,21 @@
+import _ from '../utils/elementUtil.js';
+
 class RepaymentView {
-    constructor() {
-        
-    }
+  constructor({ processModel }, { $processExchange }) {
+    this.processModel = processModel;
+    this.$processExchange = $processExchange;
+    this.init();
+  }
 
+  init() {
+    this.initEvent();
+  }
 
-
+  initEvent() {
+    _.on(this.$processExchange, 'click', () => {
+      console.log(123)
+    })
+  }
 }
 
 
