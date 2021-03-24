169adba2f3987dd0c1b6ee76fd5611a9eeaf8d1e
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-03-25 01:18:48 +0900

    Add : 상품배출 및 상품 선택 비활성화 기능 추가

--- vendingMachine/public/src/Views/ProductView.js
+++ vendingMachine/public/src/Views/ProductView.js
@@ -11,8 +11,13 @@ class ProductView {
     this.init();
   }
 
+  notify(content) {
+    console.log(`모델의 ${content} 가 바뀌었어`); // ProductModel에서 데이터가 변경되면 알림받음
+  }
+  
   init() {
     this.initEvent();
+    this.productModel.registerObserver(this); // ProductView 가 ProductModel을 구독
   }
 
   initEvent() {
@@ -22,8 +27,23 @@ class ProductView {
   clickProductHandler({ target }) {
     if (!target.classList.contains(this.nameListClassName)) return;
     this.productModel.sold(target.innerText);
-    this.render();
+
+    // 상품이 판매중일때만 제품 배출
+    if(!this.isSoldOut(target)) this.disposeProduct(target);
+    else console.log("매진된 제품입니다.");
   }
+
+  isSoldOut(product) {
+    return product.classList.contains("disable");
+  }
+
+  disposeProduct(product) {
+    setTimeout(() => {
+      console.log(`${product.innerText} 뽑았습니다.`);
+      this.render();
+    },2000)
+  }
+
 
   itemDisableChanger() {
     const pairs = zip(this.$nameList, this.productModel.getProduct());
