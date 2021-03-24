6175a44720cfa3f0b166930e0dbf751f9b7bd71f
jeonyeonkyu <ehb6915@naver.com>
2021-03-25 01:18:10 +0900

    Add: Model, element 넣어주고 틀잡기

--- vendingMachine/public/src/Views/InputAmountView.js
+++ vendingMachine/public/src/Views/InputAmountView.js
@@ -1,8 +1,17 @@
 class InputAmountView {
-    constructor() {
-        
-    }
+  constructor({ processModel }, { $processPrice }) {
+    this.processModel = processModel;
+    this.$processPrice = $processPrice;
+    this.init();
+  }
 
+  init() {
+    this.processModel.subscribe(this.render.bind(this));
+  }
+
+  render({ money }) {
+
+  }
 
 
 }
