0f4fc76c0320d4f934b62c88e595b77d63680f9b
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-03-25 15:16:41 +0900

    Add : 상품 비활성화 및 배출 기능 추가

--- vendingMachine/public/src/Models/ProductModel.js
+++ vendingMachine/public/src/Models/ProductModel.js
@@ -1,30 +1,16 @@
 class ProductModel {
   constructor({ products }) {
     this.products = products;
-    this.observers = [];
   }
 
   sold(name) {
     const item = this.products.find(item => item.name === name);
     item.count--;
-    this.notifyObservers(name);
   }
 
   getProduct() {
     return [...this.products];
   }
-
-  registerObserver(target) {
-    this.observers.push(target);
-  }
-
-  unregisterObserver(target) {
-    this.observers = this.observers.filter(observer => observer !== target); 
-  }
-
-  notifyObservers(content) {
-    this.observers.forEach((observer) => observer.notify(content));
-  } 
 
 }
 
