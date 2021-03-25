063860792dfa43444d636e3dd9fa9e2714d6c724
jeonyeonkyu <ehb6915@naver.com>
2021-03-25 15:16:26 +0900

    Feat: processObject의 money를 수정하는 메서드 추가

--- vendingMachine/public/src/Models/ProcessModel.js
+++ vendingMachine/public/src/Models/ProcessModel.js
@@ -3,20 +3,28 @@ import Observable from '../Observable/Observable.js';
 class ProcessModel extends Observable {
   processObject = {
     money: 0,
-    exchange: false,
     log: []
   }
 
   constructor() {
     super();
-    
   }
-
 
   getProcessObject() {
     return this.processObject;
   }
 
+  getMoney() {
+    return this.processObject.money;
+  }
+
+  exchangeMoney() {
+    this.processObject.money = 0;
+  }
+
+  updateMoney(money) {
+    this.processObject.money += money;
+  }
 
 }
 
