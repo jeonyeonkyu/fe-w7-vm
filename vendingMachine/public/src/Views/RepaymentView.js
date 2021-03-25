1ce3f490d475b65b2aca5be5a33cf5ec7bfa05f9
jeonyeonkyu <ehb6915@naver.com>
2021-03-25 15:20:19 +0900

    Feat: exchangeClickHandler 추가 (반환버튼 클릭시 동작)

--- vendingMachine/public/src/Views/RepaymentView.js
+++ vendingMachine/public/src/Views/RepaymentView.js
@@ -1,7 +1,8 @@
 import _ from '../utils/elementUtil.js';
 
 class RepaymentView {
-  constructor({ processModel }, { $processExchange }) {
+  constructor({ walletModel, processModel }, { $processExchange }) {
+    this.walletModel = walletModel;
     this.processModel = processModel;
     this.$processExchange = $processExchange;
     this.init();
@@ -12,18 +13,19 @@ class RepaymentView {
   }
 
   initEvent() {
-    _.on(this.$processExchange, 'click', () => {
-      console.log(123)
-    })
+    _.on(this.$processExchange, 'click', this.exchangeClickHandler.bind(this));
+  }
+
+  exchangeClickHandler() {
+    const processObj = this.processModel.getProcessObject();
+    const inputMoney = this.processModel.getMoney();
+    this.processModel.exchangeMoney();
+    this.processModel.notify(processObj);
+    this.walletModel.addMoney(inputMoney);
+    const wallet = this.walletModel.getWallet();
+    this.walletModel.notify(wallet);
   }
 }
-
-
-
-
-
-
-
 
 
 export default RepaymentView;
